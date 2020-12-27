e970c2e35be4c03fa0a15879832fa00a4620fea9
JeetSolanki999 <jeetsolanki3012@gmail.com>
2020-12-27 13:41:35 +0300

    setting up

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,9 @@ import Firebase, { FirebaseContext } from './firebase';
 
 ReactDOM.render(
     <React.StrictMode>
-        <FirebaseContext.Provider value={new Firebase()}>
-            <App />
-        </FirebaseContext.Provider>
+        Hello
+        <h1>Hello</h1>
+        <App />
     </React.StrictMode>,
     document.getElementById('root'),
 );
---
+++ src/pages/sign-in screen/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { TextField } from '@material-ui/core';
+
+export default class SignInScreen extends React.Component {
+    public render(): JSX.Element {
+        return <div></div>;
+    }
+}
+
+class PageTwo extends React.Component {
+    public render(): JSX.Element {
+        return <div></div>;
+    }
+}
