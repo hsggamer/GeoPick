361f011602b2a677f7327d20d3f64cf809ecc48e
syedkhajahussainsa <55559578+syedkhajahussainsa@users.noreply.github.com>
2020-12-28 06:08:55 +0400

    sign-up-screen update

--- src/pages/sign-up screen/SignUpScreen.tsx
+++ src/pages/sign-up screen/SignUpScreen.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
-
+import './pages/sign-SignUpScreen/styles.scss';
 export interface SignUpProps {}
 
 export default class SignUpScreen extends React.Component<SignUpProps> {
     public render(): JSX.Element {
-        return <div></div>;
+        return ( <html>
+            <body>
+            <div className="App">
+             <h1> <span>Sign</span> Up</h1>
+            <div className ="Sign-Up">
+              <p>Enter some of your <br></br> <span>personal details</span>...</p>
+              <form>
+                <input type="text" className="form" placeholder="Email"/><br></br>
+                <input type="password" className="form" placeholder="Password" required/><br></br>
+                <input type="password" className="form" placeholder="Confirm password" required/><br></br>
+                <input type="radio" className="radio-btn"/><span>  By signing up i conform that i have read and accepted the terms and conditions of using the application.</span><br></br>
+              <button className="signin-btn">Sign Up!</button>
+                </form> 
+        
+             </div>
+           </div>
+        </body>
+        </html>);
     }
 }
